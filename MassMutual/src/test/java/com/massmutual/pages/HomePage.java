e29e10a787832924eb1c6e8d4ff37d827be9b83c
sashankvedula <83942411+sashankvedula@users.noreply.github.com>
2021-05-10 20:28:56 +0530

    Updated the Changes

--- MassMutual/src/test/java/com/massmutual/pages/HomePage.java
+++ MassMutual/src/test/java/com/massmutual/pages/HomePage.java
@@ -4,6 +4,9 @@ import java.util.List;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+import org.testng.Assert;
+
+import com.massmutual.utilities.Utilities;
 
 public class HomePage extends BasePage{
 
@@ -19,11 +22,121 @@ public class HomePage extends BasePage{
 	@FindBy(xpath = "//*[starts-with(@id,'txt_val')]")
 	private List<WebElement> listOfAmounts;
 	
+	@FindBy(xpath = "//*[starts-with(@id,'txt_amt')]")
+	private WebElement amounts;
+	
 	@FindBy(id = "lbl_ttl_val")
 	private WebElement totalBalanceLabel;
 
 	@FindBy(id = "txt_ttl_val")
 	private WebElement totalBalanceAmount;
+	
+	@FindBy(id = "txt_ttl_curr")
+	private WebElement currency;
+	
+	/*
+	 * Getter Methods to access the elements
+	 */
+	
+	
+	public List<WebElement> getListOfLabels() {
+		return ListOfLabels;
+	}
+
+	public List<WebElement> getListOfAmounts() {
+		return listOfAmounts;
+	}
+	
+	public WebElement getAmounts() {
+		return amounts;
+	}
+
+	public WebElement getTotalBalanceLabel() {
+		return totalBalanceLabel;
+	}
+
+	public WebElement getTotalBalanceAmount() {
+		return totalBalanceAmount;
+	}
+	
+	public WebElement getCurrency() {
+		return currency;
+	}
+
+	
+	
+			public void amountsValidator() {
+				
+				if(getAmounts().isDisplayed()) {
+					Assert.assertTrue(true);
+				}
+				
+				else {
+					Assert.assertFalse(false);
+				}
+			} 
+	
+			public void amountGreaterThan(int number) {
+				
+				int sum =0;
+				for(int i=0;i<getListOfAmounts().size();i++) {
+					
+					
+					String val = getListOfAmounts().get(i).getText();
+					int amt = Integer.parseInt(val);
+					sum = sum+amt;
+				}
+				
+				if(sum >number) {
+					Assert.assertTrue(true);
+				}
+				
+				else {
+					Assert.assertFalse(false);
+				}
+			}
+	
+			public void countOfAmounts(int number) {
+					
+					if(getListOfAmounts().size()>number) {
+						Assert.assertTrue(true);
+					}
+					
+					else {
+						Assert.assertFalse(false);
+					}
+				}
+			
+			public void currencyFormatOfAmount() {
+				
+				if(getCurrency().isDisplayed()) {
+					Assert.assertTrue(getCurrency().equals("currency"),"Pass");
+				}
+				
+				else {
+					Assert.assertFalse(false);
+				}
+			}
+			
+			public void totalBalanceAndValues() {
+				
+				if(getTotalBalanceAmount().equals(getTotalBalanceLabel())) {
+					Assert.assertTrue(true);
+				}
+				
+				else {
+					Assert.assertFalse(false);
+				}
+							
+							
+						}
+			
+
+		
+	
+
+	
+	
 	
 	
 	
--- MassMutual/src/test/java/com/massmutual/steps/BaseSteps.java
+++ MassMutual/src/test/java/com/massmutual/steps/BaseSteps.java
@@ -3,6 +3,8 @@ package com.massmutual.steps;
 
 
 import com.massmutual.pages.BasePage;
+import com.massmutual.pages.HomePage;
+
 import io.cucumber.java.After;
 import io.cucumber.java.Before;
 import io.cucumber.java.en.Then;
@@ -11,6 +13,7 @@ import io.cucumber.java.en.When;
 public class BaseSteps {
 	
 	BasePage page = new BasePage();
+	HomePage homePage = new HomePage();
 	
 	@Before
 	public void setUp() {
@@ -20,29 +23,29 @@ public class BaseSteps {
 	
 	@When("amounts are present")
 	public void amounts_are_present() {
-	    
+	    homePage.amountsValidator();
 	}
 
 	@Then("count should be {int}")
 	public void count_should_be(Integer int1) {
-	   
+		homePage.countOfAmounts(5);
 	}
 
 	@Then("amounts should be greater than {int}")
 	public void amounts_should_be_greater_than(Integer int1) {
-	    
+	    homePage.amountGreaterThan(222);
 	    
 	}
 
 	@Then("amounts should be displayed as currencies {string}")
 	public void amounts_should_be_displayed_as_currencies(String string) {
 	    
-	    
+	    homePage.currencyFormatOfAmount();
 	}
 
 	@Then("total balance should be equal to sum of the values")
 	public void total_balance_should_be_equal_to_sum_of_the_values() {
-	   
+		homePage.totalBalanceAndValues();
 	}
 	
 	@After
